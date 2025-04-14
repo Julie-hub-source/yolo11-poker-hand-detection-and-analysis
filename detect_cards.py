b44e55b276eb0087d09a912c6a146600c6d9d78d
Nguyen Thi Cam Thach <thach.nguyen.220226@student.fulbright.edu.vn>
2025-04-14 21:52:15 +0700

    Update detect_cards.py

--- detect_cards.py
+++ detect_cards.py
@@ -17,7 +17,8 @@ def detect_cards(image_path, weights_path, conf=0.5, save_output=True):
         list: List of unique cards detected in the image, sorted left to right.
     '''
     model = YOLO(weights_path, task='detect')
-    results = model.predict(image_path, save=False, conf=conf)[0]
+    result = model.predict(source=image_path, conf=conf, save=False)[0]
+
     
     cards = []
     card_names = []
